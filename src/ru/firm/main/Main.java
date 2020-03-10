def06be2d0b8f22374fc709e2da9e3215f53948c
Sergey Pertsev <s.pertsev@outlook.com>
2020-03-11 01:11:04 +0300

    Added package 'resources' and files 'encrypted_text.txt' and 'alphabet.txt'. Implemented class SimpleFileRepository. Removed class SourceData.

--- src/ru/firm/main/Main.java
+++ src/ru/firm/main/Main.java
@@ -1,18 +1,20 @@
 package ru.firm.main;
 
+import ru.firm.main.repository.SimpleFileRepository;
 import ru.firm.main.service.Decoder;
 import ru.firm.main.service.InteractRunner;
-import ru.firm.main.repository.SourceData;
 
 public class Main {
 
     public static void main(String[] args) {
-        SourceData sourceData = new SourceData();
+
+        SimpleFileRepository simpleFileRepository = new SimpleFileRepository();
         InteractRunner interactRunner = new InteractRunner();
         Decoder decoder = new Decoder();
 
-        interactRunner.interactWithUser(sourceData.getText());
-        decoder.decryptText(sourceData.getText(), sourceData.getAlphabet());
+        interactRunner.interactWithUser(simpleFileRepository.readFileFromResources("encrypted_text.txt"));
+        decoder.decryptText(simpleFileRepository.readFileFromResources("encrypted_text.txt"), simpleFileRepository.readFileFromResources("alphabet.txt"));
+
     }
 
 }
